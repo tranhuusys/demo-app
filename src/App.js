133026c083e93a6e9ba71a771bcc1cea1c3271c4
Sỹ Trần <tranhuusys@Sys-MBPro.local>
2023-02-17 11:02:37 +0700

    demo is ready

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World,
-        </p>
+        <p>Hello World!</p>
+        <img
+          src={require("./meme-la-gi-meme-bat-nguon-tu-dau-4bf9f9de.png")}
+          // className="App-logo"
+          alt="logo"
+        />
       </header>
     </div>
   );
